98ad00dcfa793ca8d055b4fe7974d9160810b10e
Zephyr Shannon <earthlingzephyr@gmail.com>
2021-02-20 12:52:55 -0800

    Create a shared state LanguageDirectory for the app

--- src/bin/dapsd.rs
+++ src/bin/dapsd.rs
@@ -1,9 +1,29 @@
-use std::path::PathBuf;
+use std::{collections::HashMap, path::PathBuf, sync::Arc};
 
+use async_std::sync::RwLock;
 use tide::prelude::*;
 use tide::Request;
 
-#[derive(Debug, Deserialize)]
+#[derive(Clone, Debug)]
+struct LanguageDirectory {
+    languages: Arc<RwLock<HashMap<String, Language>>>,
+}
+
+impl Default for LanguageDirectory {
+    fn default() -> LanguageDirectory {
+        LanguageDirectory {
+            languages: Arc::default(),
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+struct Language {
+    name: String,
+    projects: HashMap<String, Project>,
+}
+
+#[derive(Debug, Default, Deserialize)]
 #[serde(rename_all = "kebab-case")]
 struct Project {
     language: String,
@@ -13,13 +33,14 @@ struct Project {
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    let mut app = tide::new();
-    app.at("/register/dir").post(register_dir);
+    let mut app = tide::with_state(LanguageDirectory::default());
+    app.at("/api/register/dir").post(register_dir);
+    app.at("/*").all(serve_page);
     app.listen("127.0.10.1:8080").await?;
     Ok(())
 }
 
-async fn register_dir(mut req: Request<()>) -> tide::Result {
+async fn register_dir(mut req: Request<LanguageDirectory>) -> tide::Result {
     println!("{:?}", req.header_names());
     println!("{:?}", req.header("host"));
     let Project {
@@ -32,4 +53,8 @@ async fn register_dir(mut req: Request<()>) -> tide::Result {
         project_name, language, directory
     )
     .into())
+}
+
+async fn serve_page(mut _req: Request<LanguageDirectory>) -> tide::Result {
+    Ok(format!("string").into())
 }
