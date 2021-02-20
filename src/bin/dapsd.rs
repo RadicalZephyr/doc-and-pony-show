4257f8db227ff8e458e80db70b87ce8c42c24502
Zephyr Shannon <earthlingzephyr@gmail.com>
2021-02-20 12:05:26 -0800

    Add a first pass web service version of the daps daemon

--- src/bin/dapsd.rs
+++ src/bin/dapsd.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, daemon world!");
+use std::path::PathBuf;
+
+use tide::prelude::*;
+use tide::Request;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+struct Project {
+    language: String,
+    project_name: String,
+    directory: PathBuf,
+}
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+    app.at("/register/dir").post(register_dir);
+    app.listen("127.0.10.1:8080").await?;
+    Ok(())
+}
+
+async fn register_dir(mut req: Request<()>) -> tide::Result {
+    println!("{:?}", req.header_names());
+    println!("{:?}", req.header("host"));
+    let Project {
+        language,
+        project_name,
+        directory,
+    } = req.body_json().await?;
+    Ok(format!(
+        "Registered {} with language {} located at {:?}",
+        project_name, language, directory
+    )
+    .into())
 }
