0acda0b4897ba0cdea56b1fea0cad3fa23b45d9b
Zephyr Shannon <earthlingzephyr@gmail.com>
2021-02-20 15:24:50 -0800

    Rework top level state types to allow moving error handling to a specific method

--- src/bin/dapsd.rs
+++ src/bin/dapsd.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, path::PathBuf, sync::Arc};
 
-use async_std::sync::{RwLock, RwLockReadGuard};
+use async_std::sync::RwLock;
 use tide::{http::headers::HeaderValues, prelude::*};
 use tide::{Request, StatusCode};
 
@@ -34,24 +34,23 @@ impl LanguageName {
     }
 }
 
+type SharedLanguageDirectory = Arc<RwLock<LanguageDirectory>>;
+
 type LanguageMap = HashMap<String, Language>;
 
-#[derive(Clone, Debug)]
+#[derive(Debug, Default)]
 struct LanguageDirectory {
-    languages: Arc<RwLock<LanguageMap>>,
-}
-
-impl Default for LanguageDirectory {
-    fn default() -> LanguageDirectory {
-        LanguageDirectory {
-            languages: Arc::default(),
-        }
-    }
+    languages: LanguageMap,
 }
 
 impl LanguageDirectory {
-    async fn languages(&self) -> RwLockReadGuard<'_, LanguageMap> {
-        self.languages.read().await
+    fn language(&self, language_name: &LanguageName) -> tide::Result<&Language> {
+        self.languages
+            .get(language_name.as_str())
+            .ok_or(tide::Error::from_str(
+                StatusCode::NotFound,
+                "language has no registered projects",
+            ))
     }
 }
 
@@ -79,14 +78,14 @@ struct Project {
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    let mut app = tide::with_state(LanguageDirectory::default());
+    let mut app = tide::with_state(SharedLanguageDirectory::default());
     app.at("/api/register/dir").post(register_dir);
     app.at("/:project_name/*path").all(serve_page);
     app.listen("127.0.10.1:8080").await?;
     Ok(())
 }
 
-async fn register_dir(mut req: Request<LanguageDirectory>) -> tide::Result {
+async fn register_dir(mut req: Request<SharedLanguageDirectory>) -> tide::Result {
     let Project {
         language,
         project_name,
@@ -99,18 +98,13 @@ async fn register_dir(mut req: Request<LanguageDirectory>) -> tide::Result {
     .into())
 }
 
-async fn serve_page(req: Request<LanguageDirectory>) -> tide::Result {
+async fn serve_page(req: Request<SharedLanguageDirectory>) -> tide::Result {
     let language_name = LanguageName::from_host_name(req.header("host"))?;
     let project_name = req.param("project_name")?;
     let path = req.param("path")?;
     let state = req.state();
-    let languages = state.languages().await;
-    let language = languages
-        .get(language_name.as_str())
-        .ok_or(tide::Error::from_str(
-            StatusCode::NotFound,
-            "language has no items",
-        ))?;
+    let language_directory = state.read().await;
+    let language = language_directory.language(&language_name)?;
     let project = language.project(project_name).ok_or(tide::Error::from_str(
         StatusCode::NotFound,
         "Project not found",
